a2d090e99e2a327e6f732586dd338bcc1d11a1a8
Rasmus Hugosson <rasmus.hugosson01@gmail.com>
2023-03-28 21:53:33 +0200

    Add project structure

---
+++ premake5.lua
@@ -0,0 +1,63 @@
+
+workspace "AerideusMem"
+    architecture "x64"
+    configurations { "Debug", "Release", "Dist" }
+
+    filter "system:windows"
+        defines { "AE_WINDOWS" }
+
+    filter "system:macosx"
+        defines { "AE_MACOS" }
+
+    filter "system:linux"
+        defines { "AE_LINUX" }
+
+    filter "configurations:Debug"
+        defines { "AE_DEBUG" }
+        symbols "On"
+
+    filter "configurations:Release"
+        defines { "AE_RELEASE" }
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines { "AE_DIST" }
+        optimize "Speed"
+        symbols "Off"
+
+    filter "action:vs*"
+        startproject "Sandbox"
+
+project "AerideusLog1_0"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++17"
+    targetdir "bin/%{cfg.buildcfg}"
+    objdir "obj/%{cfg.buildcfg}"
+    
+    files { "AerideusLog1_0/src/*.c", "AerideusLog1_0/include/*.h", "AerideusLog1_0/internal/*.h" }
+
+
+project "AerideusMem"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++17"
+    targetdir "bin/%{cfg.buildcfg}"
+    objdir "obj/%{cfg.buildcfg}"
+
+    files { "AerideusMem/src/*.c", "AerideusMem/include/*.h", "AerideusMem/internal/*.h" }
+
+    includedirs { "AerideusLog1_0/include" }
+    links { "AerideusLog1_0" }
+
+project "Sandbox"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++17"
+    targetdir "bin/%{cfg.buildcfg}"
+    objdir "obj/%{cfg.buildcfg}"
+  
+    files { "Sandbox/src/*.c", "Sandbox/src/*.h" }
+
+    includedirs { "AerideusMem/include" }
+    links { "AerideusMem" }
